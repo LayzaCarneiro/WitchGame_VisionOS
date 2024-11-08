67f87d8cf1b481eb9f1a71e64b894aec864c77de
Layza Carneiro <layza.mrcarneiro@gmail.com>
2024-11-08 10:41:03 -0300

    Add behaviors to potion1, potion2, potion3

--- TestePotion/ContentView.swift
+++ TestePotion/ContentView.swift
@@ -19,16 +19,10 @@ struct ContentView: View {
             if let scene = try? await Entity(named: "Scene", in: realityKitContentBundle) {
                 content.add(scene)
             }
-        } update: { content in
-            // Update the RealityKit content when SwiftUI state changes
-            if let scene = content.entities.first {
-                let uniformScale: Float = enlarge ? 1.4 : 1.0
-                scene.transform.scale = [uniformScale, uniformScale, uniformScale]
-            }
         }
-        .gesture(TapGesture().targetedToAnyEntity().onEnded { _ in
-            enlarge.toggle()
-        })
+        .gesture(TapGesture().targetedToAnyEntity().onEnded({ value in
+            _ = value.entity.applyTapForBehaviors()
+        }))
 
     }
 }
