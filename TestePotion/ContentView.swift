56d1361410318c6886ff90d0fe20e9071c8214e0
Layza Carneiro <layza.mrcarneiro@gmail.com>
2024-11-09 23:09:29 -0300

    Add decoration to scenario

--- TestePotion/ContentView.swift
+++ TestePotion/ContentView.swift
@@ -10,20 +10,49 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
-
-    @State private var enlarge = false
+    @State private var showText = false
+    @State private var clickedEntities: Set<UUID> = []
+    @State private var count = 0
 
     var body: some View {
         RealityView { content in
             // Add the initial RealityKit content
             if let scene = try? await Entity(named: "Scene", in: realityKitContentBundle) {
+                scene.transform.scale = SIMD3<Float>(0.82, 0.82, 0.82)
                 content.add(scene)
             }
         }
         .gesture(TapGesture().targetedToAnyEntity().onEnded({ value in
             _ = value.entity.applyTapForBehaviors()
+            handleTap()
         }))
-
+        .gesture(DragGesture().targetedToAnyEntity())
+        
+        
+        if showText {
+            Text("Hello, World!")
+                .font(.headline)
+                .padding()
+                .background(Color.white.opacity(0.8))
+                .cornerRadius(10)
+        }
+    }
+    
+    
+    private func handleTap() {
+        let entityUUID = UUID()
+        if !clickedEntities.contains(entityUUID) {
+            clickedEntities.insert(entityUUID)
+            count += 1
+            print("clicou entidade, contador: \(count)")
+            
+        } else {
+            print("entidade já clicada, contador: \(count)")
+        }
+        
+        if count >= 3 {
+            showText = true
+        }
     }
 }
 
